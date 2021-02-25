407302ed0b5c9756254aa080b4223f34e709b752
wcgcyx <wcgcyx@gmail.com>
2021-02-25 16:40:01 +1000

    Update offer_publish_handler.go

--- internal/api/adminapi/offer_publish_handler.go
+++ internal/api/adminapi/offer_publish_handler.go
@@ -3,6 +3,8 @@ package adminapi
 import (
 	"net/http"
 
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/cidoffer"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrcrypto"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrmessages"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/logging"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/nodeid"
@@ -13,7 +15,21 @@ import (
 
 func handleProviderPublishGroupCID(w rest.ResponseWriter, request *fcrmessages.FCRMessage, c *core.Core) {
 	logging.Info("handleProviderPublishGroupCID: %+v", request)
-	_, offer, _ := fcrmessages.DecodeProviderPublishGroupCIDRequest(request)
+	_, dummyOffer, _ := fcrmessages.DecodeProviderPublishGroupCIDRequest(request)
+
+	// Create & Sign the offer
+	offer, err := cidoffer.NewCidGroupOffer(dummyOffer.NodeID, &dummyOffer.Cids, dummyOffer.Price, dummyOffer.Expiry, dummyOffer.QoS)
+	if err != nil {
+		logging.Error("Error in creating offer.")
+		return
+	}
+	err = offer.SignOffer(func(msg interface{}) (string, error) {
+		return fcrcrypto.SignMessage(c.ProviderPrivateKey, c.ProviderPrivateKeyVersion, msg)
+	})
+	if err != nil {
+		logging.Error("Error in signing the offer.")
+		return
+	}
 
 	c.RegisteredGatewaysMapLock.RLock()
 	defer c.RegisteredGatewaysMapLock.RUnlock()
